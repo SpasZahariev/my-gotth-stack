0797ef3490f6a7757f058401c6679e55f28efdf7
spas.z <spaszahariev54@gmail.com>
2024-10-14 00:04:30 +0200

    updating things that didn't work when I forked the repo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,29 @@
-const colors = require('tailwindcss/colors')
+// const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    'internal/templates/*.templ',
-  ],
+  content: ["internal/templates/*.templ"],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: "1rem",
-        mobile: "2rem",
-        tablet: "4rem",
-        desktop: "5rem",
-      },
-    },
+    // container: {
+    //   center: true,
+    //   padding: {
+    //     DEFAULT: "1rem",
+    //     mobile: "2rem",
+    //     tablet: "4rem",
+    //     desktop: "5rem",
+    //   },
+    // },
     extend: {
-      colors: {
-        primary: colors.blue,
-        secondary: colors.yellow,
-        neutral: colors.gray,
-      }
+      // colors: {
+      //   primary: colors.blue,
+      //   secondary: colors.yellow,
+      //   neutral: colors.gray,
+      // }
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/typography'),
+  ],
+};
+
